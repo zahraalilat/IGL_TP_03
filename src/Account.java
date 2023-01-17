f4cacd17d345907ce8f26f038d07916b0c16fe2e
zahraalilat <100164609+zahraalilat@users.noreply.github.com>
2023-01-17 12:19:10 +0100

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -3,10 +3,16 @@ package Library_Managment;
 public class Account {
 
 	private String no_book_borrowed;
-
+ private int a ;
 	private String no_book_lost;
 
 	private double fine_amount;
+ // modification 
+	
+ public Account (String no_book_borrowed , int a){
+ this.no_book_borrowed =no_book_borrowed;
+	this.a=a;
+	}
 
 	public void calculate_fine() {
 
